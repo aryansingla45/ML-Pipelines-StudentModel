e33c84782616541c5a71362e3051ab7b5c428c85
Aryan Singla <62651987+aryansingla45@users.noreply.github.com>
2024-05-24 16:03:46 +0000

    updated app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask , render_template, request
 import numpy as np
 import pandas as pd
+
 from sklearn.preprocessing import StandardScaler
-import pickle
+from src.pipeline.predict_model import CustomData,PredictPipeline
+
 
 app = Flask(__name__)
 
@@ -12,11 +14,28 @@ def index():
 
 @app.route('/predictdata', methods=['POST' , 'GET'])
 def predict_datapoints():
-    if request.method == "GET":
+    if request.method=='GET':
         return render_template('home.html')
     else:
-        pass
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
 
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
 
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
